9470d35053c30d3f7419c418653188b962aaa0e7
Raja Kapur <raja.kapur@gmail.com>
2014-01-07 05:47:39 -0500

    Added key() to strpos

--- src/iter.filter.php
+++ src/iter.filter.php
@@ -3,13 +3,13 @@
 namespace iter\filter;
 
 function prefix($string) {
-    return function($target) use ($string) {
-        return (strpos($target, $string) === 0);
+    return function(array $target) use ($string) {
+        return (strpos(key($target), $string) === 0);
     };
 }
 
 function contains($string) {
-    return function($target) use ($string) {
-        return (strpos($target, $string) !== false);
+    return function(array $target) use ($string) {
+        return (strpos(key($target), $string) !== false);
     };
 }
